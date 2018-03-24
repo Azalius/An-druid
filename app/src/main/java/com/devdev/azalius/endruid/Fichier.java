8e086b17b9f9dd5c3162d3b11156cfaf117dd6f6
Azalius <mateo.enzo@yahoo.fr>
2018-03-24 13:34:25 +0100

    un exemple de onclicklistener pour copier en cadeau

--- app/src/main/java/com/devdev/azalius/endruid/Fichier.java
+++ app/src/main/java/com/devdev/azalius/endruid/Fichier.java
@@ -3,6 +3,7 @@ package com.devdev.azalius.endruid;
 import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -17,15 +18,27 @@ public class Fichier implements Gelement {
     private final PathManager fp;
     private File fic;
     private Context ct;
+    FileExplorer fe;
 
     public Fichier(File fic, Context ct, PathManager fp, FileExplorer fileExplorer) {
         this.fp = fp;
         this.fic = fic;
         this.ct = ct;
+        this.fe  = fileExplorer;
     }
 
     @Override
     public View toDisplay() {
+
+        /*exemple
+        btnCopy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               fe.setCopy(fic.getAbsolutePath());
+            }
+        });
+        */
+
         LinearLayout ll = new LinearLayout(this.ct);
 
         TextView tv = new TextView(this.ct);
